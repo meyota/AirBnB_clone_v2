73a02132237b37b6b32c8c5efd4ae9df6027a1d4
meyota <meyota08@gmail.com>
2022-11-20 11:49:35 +0000

    DBStorage_States_and_Cities

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,10 @@
 import models
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DATETIME
+from models import storage_type
+
 
 Base = declarative_base()
 
